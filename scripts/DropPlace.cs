5ecfaac5261cdd5b65be79dccbb06c86b9c067f0
DangerKrip <71184374+DangerKrip@users.noreply.github.com>
2020-10-31 16:35:11 +0200

    Add files via upload

---
+++ scripts/DropPlace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DropPlace : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        CardScript card = eventData.pointerDrag.GetComponent<CardScript>();
+
+        if(card)
+        {
+            card.DefaultParent = transform;
+        }
+    }
+}
